08ecef3be9a35eae2aeb5ac2c66967f8693ba088
tmadden <tmadden@anl.gov>
2017-08-16 17:11:12 -0500

    add fftAnal to reflect new fw

--- Roach2DevelopmentTree/pyfiles/fftAnalyzerR2.py
+++ Roach2DevelopmentTree/pyfiles/fftAnalyzerR2.py
@@ -909,7 +909,7 @@ class fftAnalyzerR2:
             #new 24bit FRD
             #self.mainFW = ROACH_DIR+'/Roach2DevelopmentTree/bestBitFiles/tesd_2017_Aug_04_1511.bof'
             #new fifo 64, fifofsmph4, 16bit frd
-            #self.mainFW = ROACH_DIR+'/Roach2DevelopmentTree/bestBitFiles/tesd_2017_Aug_15_1158.bof'
+            self.mainFW = ROACH_DIR+'/Roach2DevelopmentTree/bestBitFiles/tesd_2017_Aug_16_1507.bof'
             
 
             self.temppath = ROACH_DIR+'/temp/'
